314e08ea86eb2aecd79e7c6f3d05ece359a8c501
Josemar <junioraguiar255@gmail.com>
2019-04-07 16:05:00 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -17,13 +19,17 @@ public class Main extends Application {
 			ScrollPane scrolPane = loader.load();
 			scrolPane.setFitToHeight(true);
 			scrolPane.setFitToWidth(true);
-			Scene mainScene = new Scene(scrolPane);
+			mainScene = new Scene(scrolPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
