089d91d8f7cf6af09a459432d42a8f65693c270e
Josemar <junioraguiar255@gmail.com>
2019-04-19 23:13:39 -0300

    DepartmentFormController

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,44 +9,55 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
-public class DepartmentFormController implements Initializable{
-    @FXML
-    private TextField txtId;
-    
-    @FXML
-    private TextField txtName;
-    
-    @FXML
-    private Label labelErrorName;
-	
-    @FXML
-    private Button btSave;
-    
-    @FXML
-    private Button btCancel;
-    
-    @FXML
-    public void onBtSaveAction() {
-    	System.out.println("obBtSaveAction");
-    }
-    
-    @FXML
-    public void onBtCancelAction() {
-    	System.out.println("obBtCancelAction");
-    }
-    
+public class DepartmentFormController implements Initializable {
+
+	private Department entity;
+
+	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtName;
+
+	@FXML
+	private Label labelErrorName;
+
+	@FXML
+	private Button btSave;
+
+	@FXML
+	private Button btCancel;
+
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("obBtSaveAction");
+	}
+
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("obBtCancelAction");
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
-		
+
 	}
-	
+
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
 	}
-	
-	
+
+	public void updateFormData() {
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
 
 }
